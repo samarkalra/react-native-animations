05ab44e23aeb0a28b31e0315f5add1de178121d1
samarkalra <skalra@ex2india.com>
2023-01-09 15:47:42 +0530

    Add width/height animation

--- App.tsx
+++ App.tsx
@@ -1,119 +1,65 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
-import React, {type PropsWithChildren} from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
   View,
+  StyleSheet,
+  Animated,
+  TouchableWithoutFeedback,
+  Text,
 } from 'react-native';
+import React, {useRef} from 'react';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+// Animating width and height values will effect layout
 
-const Section: React.FC<
-  PropsWithChildren<{
-    title: string;
-  }>
-> = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
+const BOX_SIZE = 150;
+
+const App = () => {
+  const animatedValue = useRef(new Animated.Value(BOX_SIZE)).current;
+
+  const animatedStyles = {
+    width: animatedValue,
+    height: animatedValue,
+  };
+
+  const startAnimation = () => {
+    Animated.timing(animatedValue, {
+      toValue: 300,
+      duration: 500,
+      //Style property 'width' and 'height' is not supported by native animated module,
+      useNativeDriver: false,
+    }).start();
+  };
+
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View style={styles.container}>
+      <TouchableWithoutFeedback onPress={startAnimation}>
+        <Animated.View style={[styles.box1, animatedStyles]}>
+          <Text>
+            This is really long text. This is really long text. This is really
+            long text. This is really long text. This is really long text. This
+            is really long text. This is really long text. This is really long
+            text.
+          </Text>
+        </Animated.View>
+      </TouchableWithoutFeedback>
+      <View style={styles.box2} />
     </View>
   );
 };
 
-const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
-
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  box1: {
+    // width: BOX_SIZE,
+    // height: BOX_SIZE,
+    backgroundColor: 'tomato',
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  box2: {
+    width: BOX_SIZE,
+    height: BOX_SIZE,
+    backgroundColor: 'blue',
   },
 });
 
