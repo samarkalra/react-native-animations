f7bb3c65aae7b6ad6bfc93a86e4444c2952d276f
samarkalra <skalra@ex2india.com>
2023-01-09 16:55:43 +0530

    Add absolute position animation

--- App.tsx
+++ App.tsx
@@ -1,119 +1,64 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
-import React, {type PropsWithChildren} from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
   View,
+  Animated,
+  TouchableWithoutFeedback,
+  StyleSheet,
+  Dimensions,
 } from 'react-native';
+import React, {useRef} from 'react';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const BOX_SIZE = 150;
 
-const Section: React.FC<
-  PropsWithChildren<{
-    title: string;
-  }>
-> = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
+const App = () => {
+  const animatedAbsolutePositionTopValue = useRef(
+    new Animated.Value(0),
+  ).current;
+  const animatedAbsolutePositionLeftValue = useRef(
+    new Animated.Value(0),
+  ).current;
+
+  const animatedStyles = {
+    top: animatedAbsolutePositionTopValue,
+    left: animatedAbsolutePositionLeftValue,
+  };
+
+  const startAnimation = () => {
+    Animated.parallel([
+      Animated.timing(animatedAbsolutePositionTopValue, {
+        toValue: Dimensions.get('window').height - BOX_SIZE - 24,
+        duration: 1000,
+        useNativeDriver: false,
+      }),
+      Animated.timing(animatedAbsolutePositionLeftValue, {
+        toValue: Dimensions.get('window').width - BOX_SIZE,
+        duration: 1000,
+        useNativeDriver: false,
+      }),
+    ]).start();
+  };
+
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View style={styles.container}>
+      <TouchableWithoutFeedback onPress={startAnimation}>
+        <Animated.View style={[styles.box, animatedStyles]} />
+      </TouchableWithoutFeedback>
     </View>
   );
 };
 
-const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
-
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  box: {
+    position: 'absolute',
+    width: BOX_SIZE,
+    height: BOX_SIZE,
+    backgroundColor: 'tomato',
+    borderWidth: 2,
+    borderColor: 'blue',
   },
 });
 
